def1fa10e6abf73eb611b30af0d1f34ff54ef16e
EOSHunter <huntercnewton@gmail.com>
2017-10-18 13:54:57 -0400

    challenge1 and challenge2 commit

---
+++ Challenge1/Challenge1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Challenge1
+{
+    class Program
+    {
+        //adds the user's inputs
+        public static double Add(int num1, int num2)
+        {
+            double answer = num1 + num2;
+            return answer;
+        }
+
+        //subtracts the user's input
+        public static double Minus(int num1, int num2)
+        {
+            double answer = num1 - num2;
+            return answer;
+        }
+
+        //divides the user's input
+        public static double Divide(int num1, int num2)
+        {
+            double answer = num1 / num2;
+            return answer;
+        }
+
+        //multiplies the user's input
+        public static double Multiply(int num1, int num2)
+        {
+            double answer = num1 * num2;
+            return answer;
+        }
+
+
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+            int num1;
+            string operation;
+            int num2;
+
+            Console.WriteLine("Calculator");
+
+            while (repeat)
+            {
+                Console.Write(Environment.NewLine);
+
+                Console.WriteLine("Enter your first number.");
+                try
+                {
+                    num1 = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("Select an operator: +, -, *, /");
+                try
+                {
+                    operation = Convert.ToString(Console.ReadLine());
+
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("Enter a second number.");
+                try
+                {
+                    num2 = Convert.ToInt32(Console.ReadLine());
+
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+
+                double answer = 0;
+                switch (operation)
+                {
+                    case "+":
+                        answer = Add(num1, num2);
+                        break;
+                    case "-":
+                        answer = Minus(num1, num2);
+                        break;
+                    case "*":
+                        answer = Multiply(num1, num2);
+                        break;
+                    case "/":
+                        answer = Divide(num1, num2);
+                        break;
+                    default:
+                        continue;
+                }
+
+                Console.Write(Environment.NewLine);
+
+                Console.Write(num1);
+                Console.Write(" " + operation + " ");
+                Console.Write(num2);
+                Console.Write(" = ");
+                Console.Write(answer);
+
+                Console.Write(Environment.NewLine);
+
+                Console.WriteLine("Want to use the calculator again? Y / N");
+                string userInput = Console.ReadLine().ToUpper();
+
+                if (userInput == "Y")
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye!");
+                    repeat = false;
+                }
+            }
+        }
+    }
+}
---
+++ Challenge2/Challenge2/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace Challenge2
+{
+    public class Car
+    {
+        public int NumOfTires { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var car1 = new Car()
+            {
+                NumOfTires = 4,
+                Year = 2010,
+                Make = "BMW",
+                Model = "328i"
+            };
+
+            var car2 = new Car()
+            {
+                NumOfTires = 4,
+                Year = 2017,
+                Make = "Porsche",
+                Model = "718 Cayman S"
+            };
+
+            var car3 = new Car()
+            {
+                NumOfTires = 4,
+                Year = 2018,
+                Make = "Audi",
+                Model = "R8"
+            };
+
+
+            Console.Write("The ");
+
+            Console.Write(car1.Year);
+
+            Console.Write(" " + car1.Make + " ");
+
+            Console.Write(car1.Model);
+
+            Console.Write(" has ");
+
+            Console.Write(car1.NumOfTires);
+
+            Console.Write(" tires.");
+
+            Console.WriteLine(Environment.NewLine);
+
+
+            Console.Write("The ");
+
+            Console.Write(car2.Year);
+
+            Console.Write(" " + car2.Make + " ");
+
+            Console.Write(car2.Model);
+
+            Console.Write(" has ");
+
+            Console.Write(car2.NumOfTires);
+
+            Console.Write(" tires.");
+
+            Console.WriteLine(Environment.NewLine);
+
+
+            Console.Write("The ");
+
+            Console.Write(car3.Year);
+
+            Console.Write(" " + car3.Make + " ");
+
+            Console.Write(car3.Model);
+
+            Console.Write(" has ");
+
+            Console.Write(car3.NumOfTires);
+
+            Console.Write(" tires.");
+
+            Console.Write(Environment.NewLine);
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("Would you like to create your own car? ( Y / N )");
+            string userInput = Convert.ToString(Console.ReadLine()).ToUpper();
+
+            if (userInput == "Y")
+            {
+                bool repeat = true;
+                while (repeat)
+                {
+
+                    Console.WriteLine("Please enter your car's Make:");
+
+                    string userMake = Convert.ToString(Console.ReadLine());
+
+
+                    Console.WriteLine("Please enter your car's Model:");
+
+                    string userModel = Convert.ToString(Console.ReadLine());
+
+
+                    Console.WriteLine("Please enter your car's Year:");
+
+                    int userYear = Convert.ToInt32(Console.ReadLine());
+
+
+                    Console.WriteLine("How many tires does your car have?");
+
+                    int userTires = Convert.ToInt32(Console.ReadLine());
+
+                    var userCar = new Car()
+                    {
+                        NumOfTires = userTires,
+                        Make = char.ToUpper(userMake[0]) + userMake.Substring(1), //capitalize 1st char
+                        Model = char.ToUpper(userModel[0]) + userModel.Substring(1), //capitalize 1st char
+                        Year = userYear,
+                    };
+
+                    Console.Write(Environment.NewLine);
+
+
+                    Console.Write("The ");
+
+                    Console.Write(userCar.Year);
+
+                    Console.Write(" " + userCar.Make + " ");
+
+                    Console.Write(userCar.Model);
+
+                    Console.Write(" has ");
+
+                    Console.Write(userCar.NumOfTires);
+
+                    Console.Write(" tires.");
+
+                    Console.Write(Environment.NewLine);
+
+                    Console.WriteLine("Want to build a car again?? ( Y / N )");
+                    string continueAnswer = Convert.ToString(Console.ReadLine()).ToUpper();
+                    if (continueAnswer == "Y")
+                    {
+                        repeat = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Awesome, let's do it!");
+                        repeat = false;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Alright, thanks for building with us!");
+            }
+        }
+    }
+}
